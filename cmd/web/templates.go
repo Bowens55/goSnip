6f64a06ed60c467aace61d28864e9bf9b8dfadea
brenden <brenden.owens39@gmail.com>
2025-07-18 13:51:11 -0700

    date in the footer now

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -11,10 +11,12 @@ import (
 // At the moment it only contains one field, but we'll add more
 // to it as the build progresses.
 type templateData struct {
-	Snippet  models.Snippet
-	Snippets []models.Snippet
+	CurrentYear int
+	Snippet     models.Snippet
+	Snippets    []models.Snippet
 }
 
+// define a cash to be used
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
