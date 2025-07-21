5bc9c63f1d5562044273c36379cda85a17c131db
brenden <brenden.owens39@gmail.com>
2025-07-21 12:09:10 -0700

    remove panic

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,7 +9,6 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	panic("oops! something went wrong") // Deliberate panic
 
 	snippets, err := app.snippets.Latest()
 	if err != nil {
